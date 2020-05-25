299d9f05abfa7e700a83419425d00a81bb71a3f2
travinh <vinh.v.tran@ucdenver.edu>
2020-05-25 02:49:40 -0600

    created users controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
+
+
+
     private
+    
   
     def sign_up_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :age)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validate :name, presence :true
+  
   has_many :landscapes
 end
